bc131564b1217ea1efcdad5c3a077fb8e3787e7c
slepming <siskaura06@gmail.com>
2025-09-26 17:26:39 +0300

    Indicate current workspace

--- scripts/rust/workspaces/src/main.rs
+++ scripts/rust/workspaces/src/main.rs
@@ -1,10 +1,10 @@
-use std::{ops::Range, process::Command};
+use std::{process::Command, str::FromStr};
 
 use serde::{Deserialize, Serialize};
 
 pub type Workspaces = Vec<WorkspaceElement>;
 
-#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Eq, Ord, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct WorkspaceElement {
     id: i32,
@@ -25,22 +25,35 @@ fn main() {
         .arg("-j")
         .output()
         .unwrap();
+    let current = Command::new("hyprctl")
+        .arg("activeworkspace")
+        .arg("-j")
+        .output()
+        .unwrap();
     let output = result.stdout;
     let mut parsed: Workspaces =
         serde_json::from_str(String::from_utf8(output).unwrap().as_ref()).unwrap();
+    let cur_worksp = current.stdout;
+    let current_workspace: WorkspaceElement =
+        serde_json::from_str(String::from_utf8(cur_worksp).unwrap().as_ref()).unwrap();
 
     parsed.sort();
 
     let active: Vec<i32> = parsed.iter().map(|w| w.id).collect();
-    let workspaces = 1..active.last().unwrap() + 2;
+    let workspaces = 1..active.last().unwrap() + 1;
 
     let buttons: Vec<String> = workspaces
         .map(|n| {
             let class = if active.contains(&n) {
-                "active"
+                if n == current_workspace.id {
+                    "active current"
+                } else {
+                    "active"
+                }
             } else {
-                "inactive"
-            };
+                return "".to_string();
+            }
+            .to_string();
 
             format!(
                 "(button :onclick \"hyprctl dispatch workspace\" :class \"{}\" {})",
@@ -50,7 +63,7 @@ fn main() {
         .collect();
 
     println!(
-        "(box :class \"workspaces\" :orientation \"h\" :halign \"start\" :valign \"center\" :spacing 10 {})",
+        "(box :class \"workspaces\" :orientation \"h\" :halign \"start\" :valign \"center\" :space-evenly false :spacing 20 {})",
         buttons.join(" ")
     )
 }
