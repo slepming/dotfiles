44aafa9091c3c20ee1009cc6fc4862ea964a9022
slepming <siskaura06@gmail.com>
2026-04-15 21:21:24 +0300

    nvim: added key for open implementation list

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -20,6 +20,10 @@ keymap.set("n", "<leader>j", function ()
 	vim.diagnostic.setloclist()
 end, opts)
 
+keymap.set("n", "<leader>i", function ()
+	vim.lsp.buf.implementation()
+end, opts)
+
 keymap.set("n", "<leader>e", "<Cmd>Explore<CR>, opts")
 vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
