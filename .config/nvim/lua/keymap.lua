b9e337317a0b5d1155296070ab87ae02cd08b2d0
slepming <siskaura06@gmail.com>
2026-04-22 21:30:19 +0300

    nvim: bind for check incoming function calls added

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -29,5 +29,6 @@ vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { n
 vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 keymap.set("n", "<leader><leader>", "<Cmd>lua FzfLua.files()<CR>", opts)
 keymap.set("n", "<leader>/", "<Cmd>FzfLua live_grep<CR>", opts)
+keymap.set("n", "<leader>c", vim.lsp.buf.incoming_calls, opts)
 
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
